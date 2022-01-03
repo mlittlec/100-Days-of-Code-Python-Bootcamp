fdf7aa63a0d52fee09b22356bb6584c6a48adc68
mlittlec <martin.littlecott@btinternet.com>
2022-01-03 15:17:16 +0000

    Day 3 code updates

---
+++ 03-Day-03/odd-or-even-check.py
@@ -0,0 +1,11 @@
+# Don't change the code below:
+number = int(input("Which number do you want to check?"))
+# Don't change the code above.
+
+# Write your code below here:
+
+if number % 2 == 0:
+    print("This is an even number")
+else:
+    print("This is an odd number")
+
