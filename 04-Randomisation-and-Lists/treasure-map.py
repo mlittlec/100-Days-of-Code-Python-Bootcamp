42d35b7663860e05a2d3b507449745783bd64724
Martin Littlecott <mlittlec@hotmail.com>
2022-01-04 22:15:00 +0000

    Create treasure-map.py

---
+++ 04-Randomisation-and-Lists/treasure-map.py
@@ -0,0 +1,10 @@
+# Don't change the code below
+row1 = [" ", " ", " "]
+row2 = [" ", " ", " "] 
+row3 = [" ", " ", " "]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure?")
+# Don't change the code above
+
+# Write your code below here
