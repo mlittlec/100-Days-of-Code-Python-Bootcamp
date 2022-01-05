fa225c66aa901199b4b7704a5ca1964774316aa3
Martin Littlecott <mlittlec@hotmail.com>
2022-01-05 10:45:19 +0000

    Update highest-score.py

--- 05-Loops/highest-score.py
+++ 05-Loops/highest-score.py
@@ -6,3 +6,13 @@ print(student_scores)
 # Don't change the code above
 
 # Write your code below here:
+
+# This uses a Python built-in - we need to use a Loop...
+#print(max(student_scores)
+student_scores = [78, 65, 89, 86, 55, 91, 64, 89]
+
+highest_score = 0
+for score in student_scores:
+  if score > highest_score:
+    highest_score = score
+print(f"The highest score in the class is: {highest_score}")
