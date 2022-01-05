7fa0983302bafc0ab841aff76c3e289a6175161b
Martin Littlecott <mlittlec@hotmail.com>
2022-01-05 10:53:26 +0000

    Create for-loop-with-range.py

---
+++ for-loop-with-range.py
@@ -0,0 +1,12 @@
+# For Loop with Range
+#   for number in range(1, 11, 3):
+#     print(number)
+
+for number in range(1, 10):
+  print(number)
+
+# Calculate the total of adding every number between 1 & 100 together...
+total = 0
+for number in range(1, 101):
+  total += number
+print(total)
