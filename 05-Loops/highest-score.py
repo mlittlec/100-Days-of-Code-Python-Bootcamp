d4dc53fc0697d5661d1cd6ec7f5e6efc186e3ffa
Martin Littlecott <mlittlec@hotmail.com>
2022-01-05 10:36:39 +0000

    Create highest-score.py

---
+++ 05-Loops/highest-score.py
@@ -0,0 +1,8 @@
+# Don't change the code below:
+student_scores = input("Input a list of student scores)
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# Don't change the code above
+
+# Write your code below here:
