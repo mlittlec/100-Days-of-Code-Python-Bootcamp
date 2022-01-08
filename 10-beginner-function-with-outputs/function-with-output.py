5e1b40780761b13ebc815c8c1222f01e103303a1
Martin Littlecott <mlittlec@hotmail.com>
2022-01-08 18:01:25 +0000

    Create function-with-output.py

---
+++ 10-beginner-function-with-outputs/function-with-output.py
@@ -0,0 +1,12 @@
+# Functions with names
+
+def format_name(fname, l_name):
+   formatted_f_name = f_name.title()
+   formatted_l_name = l_name.title()
+
+   return(f"{formatted_f_name} {formated_l_name}")
+   
+  
+  formatted_string = format_name("MARTIN", "littlecott")
+  print(formatted_string)
+  
