f77d470d68b0d6c285cb6f1acb4df6ccf683969c
Martin Littlecott <mlittlec@hotmail.com>
2022-01-08 21:36:07 +0000

    Update function-with-output.py

--- 10-beginner-function-with-outputs/function-with-output.py
+++ 10-beginner-function-with-outputs/function-with-output.py
@@ -1,10 +1,12 @@
 # Functions with names
 
 def format_name(fname, l_name):
+   if f_name == "" or l_name == "":
+      return "You didn't provide valid inputs."
    formatted_f_name = f_name.title()
    formatted_l_name = l_name.title()
 
-   return(f"{formatted_f_name} {formated_l_name}")
+   return(f"Result: {formatted_f_name} {formated_l_name}")
    
   
   formatted_string = format_name("MARTIN", "littlecott")
