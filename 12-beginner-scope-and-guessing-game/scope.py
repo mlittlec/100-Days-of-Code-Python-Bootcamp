8a595b1a9cfff00d671bf9cf553db28b4d9203a3
Martin Littlecott <mlittlec@hotmail.com>
2022-01-09 17:35:53 +0000

    Update scope.py

--- 12-beginner-scope-and-guessing-game/scope.py
+++ 12-beginner-scope-and-guessing-game/scope.py
@@ -3,8 +3,26 @@
 enemies = 1
 
 def increase_enemies():
-  enemies = 2
-  print(f"enemies inside function: {enemies}")
+    enemies = 2
+    print(f"enemies inside function: {enemies}")
 
 increase_enemies()
 print(f"enemies outside function: {enemies}")
+
+# Local scope
+
+def drink_potion():
+    potion_strength = 2
+    print(potion_strength)
+  
+drink_potion()
+
+# Global scope
+player_health = 10
+
+def drink_potion():
+    potion_strength = 2
+    print(player_health)
+  
+drink_potion()
+print(player_health)
