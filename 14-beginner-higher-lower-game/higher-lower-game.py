197dfa7cc5f32aaf9f3baf0f51beeb5aa4465ab2
Martin Littlecott <mlittlec@hotmail.com>
2022-01-09 23:07:27 +0000

    Update higher-lower-game.py

--- 14-beginner-higher-lower-game/higher-lower-game.py
+++ 14-beginner-higher-lower-game/higher-lower-game.py
@@ -1,1 +1,30 @@
 # Placeholder to get started with...
+
+# display art
+
+# Generate a random account  from the game data
+
+
+# Format the account data into a printable format
+
+
+# Ask the user for a guess.
+
+
+# Check if the user is correct.
+## Get follower count of each account
+## Use if statement to check if the user is correct
+
+
+# Give user feedback on their guess.
+
+
+# Score keeping
+
+# Make the game repeatable.
+
+
+# Make the account at position B become the next account at position A.
+
+
+# Clear the screen between iterations...
