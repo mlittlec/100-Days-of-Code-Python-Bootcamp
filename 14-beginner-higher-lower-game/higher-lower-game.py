0e46d8e3e4b91193c60fd70136d1cdb9b1f95944
Martin Littlecott <mlittlec@hotmail.com>
2022-01-10 21:05:57 +0000

    Update higher-lower-game.py

--- 14-beginner-higher-lower-game/higher-lower-game.py
+++ 14-beginner-higher-lower-game/higher-lower-game.py
@@ -1,12 +1,21 @@
-# Placeholder to get started with...
+from art import logo
+from game-data import data
+import random
 
 # display art
+print(logo)
 
-# Generate a random account  from the game data
+# Generate a random account from the game data
+account_a = random.choice(data)
+account_b = random.choice(data)
 
-
+if account_a == account_b:
+    account_b = random.choice(data)
+    
 # Format the account data into a printable format
-
+account_name = account_a["name"]
+account_descr = account_a["description"]
+account_country = account_a["country"]
 
 # Ask the user for a guess.
 
