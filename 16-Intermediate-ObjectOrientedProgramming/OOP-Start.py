bbaa6843a02468879fe9d3e3074644a9bcc058d9
Martin Littlecott <mlittlec@hotmail.com>
2022-01-11 23:19:17 +0000

    Update OOP-Start.py

--- 16-Intermediate-ObjectOrientedProgramming/OOP-Start.py
+++ 16-Intermediate-ObjectOrientedProgramming/OOP-Start.py
@@ -1,5 +1,14 @@
-import turtle
+from turtle import Turtle, Screen
 
 import another_module
 
 print(another_module.another_variable)
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
