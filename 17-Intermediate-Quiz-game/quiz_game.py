2ba2288a6126e8103eedc6bcf51e20bb5ba86189
Martin Littlecott <martin.littlecott@btinternet.com>
2022-01-12 23:30:32 +0000

    Submit updates at end of day...

--- 17-Intermediate-Quiz-game/quiz_game.py
+++ 17-Intermediate-Quiz-game/quiz_game.py
@@ -1,1 +1,13 @@
 # Placeholder
+from question_model import Question
+from data import question_data
+
+question_bank = []
+
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+print(question_bank)
