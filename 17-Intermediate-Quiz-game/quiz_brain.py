a64d9677476724dc84cc594579a056e0d6b17d74
Martin Littlecott <mlittlec@hotmail.com>
2022-01-13 20:32:29 +0000

    Update quiz_brain.py

--- 17-Intermediate-Quiz-game/quiz_brain.py
+++ 17-Intermediate-Quiz-game/quiz_brain.py
@@ -2,6 +2,7 @@
 class QuizBrain:
   def __init__(self, q_list):
       self.question_number = 0
+      self.score = 0
       self.question_list = q_list
   
   def still_has_questions(self):
@@ -10,4 +11,15 @@ class QuizBrain:
   def next_question(self):
       current_question = self.question_list[self.question_number]
       self.question_number += 1
-      input(f"q.{self.question_number}: {current_question.text} (True/False): ")
+      user_answer = input(f"q.{self.question_number}: {current_question.text} (True/False): ")
+      self.check_answer(user_answer, current_question.answer)
+  
+  def check_answer(self, user_answer, correct_answer):
+      if user.answer.lower() == correct_answer.lower():
+          self.score += 1
+          print("You got it right")
+      else:
+          print("Sadly, that's not the correct answer")
+      print(f"The correct answer was: {correct_answer}. ")
+      print(f"Your current score is: {self.score}/{self.question_number). ")
+      
