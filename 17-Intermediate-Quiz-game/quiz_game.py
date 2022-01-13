55cded2cc04cac4ab6b227388b46b10a4cd475fd
Martin Littlecott <mlittlec@hotmail.com>
2022-01-13 20:41:28 +0000

    Update quiz_game.py

--- 17-Intermediate-Quiz-game/quiz_game.py
+++ 17-Intermediate-Quiz-game/quiz_game.py
@@ -17,3 +17,6 @@ quiz = QuizBrain(question_bank)
 
 while quiz.still_has_question():
     quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{len(question_bank)}")
