e54ce8990b4b97d63bf69b63687bf31eb7515ddb
Martin Littlecott <mlittlec@hotmail.com>
2022-01-13 20:03:03 +0000

    Update quiz_brain.py

--- 17-Intermediate-Quiz-game/quiz_brain.py
+++ 17-Intermediate-Quiz-game/quiz_brain.py
@@ -1,1 +1,9 @@
 # Placeholder
+class QuizBrain:
+  def __init__(self, q_list):
+      self.question_number = 0
+      self.question_list = q_list
+  
+  def next_question(self):
+      current_question = self.question_list[self.question_number]
+      input(f"q.{self.question_number}: {current_question.text} (True/False): ")
