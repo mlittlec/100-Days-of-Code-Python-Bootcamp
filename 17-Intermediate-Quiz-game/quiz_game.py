95ad1b6af86cd333e13db2297ffa8c23a64f8951
Martin Littlecott <mlittlec@hotmail.com>
2022-01-13 20:05:31 +0000

    Update quiz_game.py

--- 17-Intermediate-Quiz-game/quiz_game.py
+++ 17-Intermediate-Quiz-game/quiz_game.py
@@ -1,6 +1,7 @@
 # Placeholder
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
@@ -10,4 +11,7 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-print(question_bank[0].answer)
+# print(question_bank[0].answer)
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
