e870e82b534f7c7cff651ef8aca9d715f0c21b7e
Martin Littlecott <mlittlec@hotmail.com>
2022-01-14 21:32:02 +0000

    Update random-walk.py

--- 18-Intermediate-Tuples-and-TurleGraphics/random-walk.py
+++ 18-Intermediate-Tuples-and-TurleGraphics/random-walk.py
@@ -2,6 +2,14 @@ import turtle as t
 import random
 
 tim = t.Turtle()
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    retrun random_color
 
 # RGB Colour Calculator : w3schools.com/colors/colors_rgb.asp
 colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
@@ -10,7 +18,8 @@ tim.pensize(10)
 tim.speed("fast")
 
 for _ in range(200):
-    tim.color(random.choice(colours)))
+    # tim.color(random.choice(colours)))
+    tim.color(random_color())
     tim.forward(30)
     tim.setheading(random.choice(directions))
     
