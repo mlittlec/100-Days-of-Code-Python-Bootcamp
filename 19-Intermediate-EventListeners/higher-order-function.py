3c3953bf0b2a62b396662c1cb6fd4d8a73257311
Martin Littlecott <mlittlec@hotmail.com>
2022-01-14 23:15:14 +0000

    Create higher-order-function.py

---
+++ 19-Intermediate-EventListeners/higher-order-function.py
@@ -0,0 +1,19 @@
+# A higher order function is a function that returns the result of invoking (calling) another function
+
+def add(n1, n2):
+    return n1 + n2
+  
+def substract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1/n2
+
+def(calculator(n1, n2, func):
+    return func(n1, n2)
+    
+result = calculator(2, 3, divide)
+print(result)
