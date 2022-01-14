f5aeec93b805293f63d5ca7e886646f683a9e926
Martin Littlecott <mlittlec@hotmail.com>
2022-01-14 22:57:46 +0000

    Create draw-Hirst-like.py

---
+++ 18-Intermediate-Tuples-and-TurleGraphics/draw-Hirst-like.py
@@ -0,0 +1,39 @@
+import turtle as turtle_module
+import random
+
+#import cologram
+
+# rgb_colors = []
+# colors = colorgram.extract('image.jpg', 30)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+    # rgb_colors.append(color.rgb)
+
+# print(rgb_colors)
+
+turtle_module.colormode(255)
+tim = turtle_module.Turtle()
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
+color_list = [(202, 164, 109), (238, 240, 245), (150, 75, 49), (223, 201, 135), (52, 93, 124), (172, 154, 40), (140, 30, 19)]
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+    
+    if dor_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
