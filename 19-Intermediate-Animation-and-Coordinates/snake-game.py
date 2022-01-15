1f8e0766c080b4495e059c9a47f555575c5d08c6
Martin Littlecott <mlittlec@hotmail.com>
2022-01-15 23:33:41 +0000

    Update snake-game.py

--- 19-Intermediate-Animation-and-Coordinates/snake-game.py
+++ 19-Intermediate-Animation-and-Coordinates/snake-game.py
@@ -5,14 +5,22 @@ screen.setup(width=600, Height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 
+starting_position = [(0, 0), (-20, 0), (-40, 0)]
 
 
+for position in starting_positions:
+    new_segment = Turtle(shape="square")
+    new_segment.color("white")
+    new_segment.goto(position)
+
+# segment_2 = Turtle(shape="square")
+# segment_2.color("white")
+# segment_2.goto(-20, 0)
 
 
-
-
-
-
+# segment_3 = Turtle(shape="square")
+# segment_3.color("white")
+# segment_3.goto(-40, 0)
 
 
 screen.exitonclick()
