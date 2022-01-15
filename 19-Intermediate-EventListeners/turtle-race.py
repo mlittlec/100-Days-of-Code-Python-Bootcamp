2e4d1b52c18e137f8ba2556d15a1cc32c91ae7e2
Martin Littlecott <mlittlec@hotmail.com>
2022-01-15 21:45:07 +0000

    Update turtle-race.py

--- 19-Intermediate-EventListeners/turtle-race.py
+++ 19-Intermediate-EventListeners/turtle-race.py
@@ -5,10 +5,14 @@ screen = Screnn()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which Turtle will win the race?  Enter a colour: ")
 print(user_bet)
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
 
-tim.turtle(shape="turtle")
-tim.penup()
-tim.goto(x=230, y=100)
+
+for turtle_index in range(9, 6):
+    tim.turtle(shape="turtle")
+    tim.penup()
+    tim.goto(x=230, y=y_positions[turtle_index])
 
 
 
