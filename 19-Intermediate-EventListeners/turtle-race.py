83726a661e66de582d0dfdd3c59294252773f6f5
Martin Littlecott <mlittlec@hotmail.com>
2022-01-15 21:03:10 +0000

    Create turtle-race.py

---
+++ 19-Intermediate-EventListeners/turtle-race.py
@@ -0,0 +1,17 @@
+import Turtle, Screen
+
+
+tim = Turtle()
+screen = Screnn()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which Turtle will win the race?  Enter a colour: ")
+
+
+
+
+
+
+
+
+screen.exitonclick()
+
