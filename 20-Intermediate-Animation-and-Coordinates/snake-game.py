c7bcf6279f0e355220da61c914b521d244d6b1e3
Martin Littlecott <martin.littlecott@btinternet.com>
2022-01-15 23:48:10 +0000

    Submit recent updates

--- 20-Intermediate-Animation-and-Coordinates/snake-game.py
+++ 20-Intermediate-Animation-and-Coordinates/snake-game.py
@@ -1,16 +1,18 @@
 from turtle import Screen, Turtle
 
 screen = Screen()
-screen.setup(width=600, Height=600)
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 
-starting_position = [(0, 0), (-20, 0), (-40, 0)]
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
+segments = []
 
 for position in starting_positions:
     new_segment = Turtle(shape="square")
     new_segment.color("white")
+    new_segment.penup()
     new_segment.goto(position)
     segments.append(new_segment)
 
