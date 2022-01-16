183d1dd4a2b28bd1a110fff3651a0e977a7936b8
Martin Littlecott <mlittlec@hotmail.com>
2022-01-16 16:18:01 +0000

    Update snake-game.py

--- 20-Intermediate-Animation-and-Coordinates/snake-game.py
+++ 20-Intermediate-Animation-and-Coordinates/snake-game.py
@@ -1,9 +1,11 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
@@ -19,6 +21,8 @@ for position in starting_positions:
 
 game_is_on = True
 while game_is_on:
+    screen.update()
+    time.sleep(0.1)
     for seg in segments:
         seg.forward(20)
 
