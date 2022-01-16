b4a4fb4a0429a2f91ea2f969a3de01f0f33634f9
Martin Littlecott <mlittlec@hotmail.com>
2022-01-16 16:48:11 +0000

    Update snake-game.py

--- 20-Intermediate-Animation-and-Coordinates/snake-game.py
+++ 20-Intermediate-Animation-and-Coordinates/snake-game.py
@@ -24,13 +24,6 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    
-    for seg_num in range(len(segments) - 1, 0, -1)
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num -1].ycor()
-        segments[segnum].goto(new_x, new_y)
-    segments[0].forward(20)
-    # segments[0].left(90)
 
 # segment_2 = Turtle(shape="square")
 # segment_2.color("white")
