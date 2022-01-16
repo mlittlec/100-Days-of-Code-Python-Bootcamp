cb68ffc8049111ad5321a54f5c8fea02af3d1dd6
Martin Littlecott <mlittlec@hotmail.com>
2022-01-16 16:47:33 +0000

    Create snake.py

---
+++ 20-Intermediate-Animation-and-Coordinates/snake.py
@@ -0,0 +1,17 @@
+from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+
+class Snake:
+  
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        
+        
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
