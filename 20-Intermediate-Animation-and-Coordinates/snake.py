e2cb68e755a812dd5974ee837d22df9202799642
Martin Littlecott <mlittlec@hotmail.com>
2022-01-16 16:52:01 +0000

    Update snake.py

--- 20-Intermediate-Animation-and-Coordinates/snake.py
+++ 20-Intermediate-Animation-and-Coordinates/snake.py
@@ -15,3 +15,11 @@ class Snake:
             new_segment.penup()
             new_segment.goto(position)
             self.segments.append(new_segment)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(20)
+        
