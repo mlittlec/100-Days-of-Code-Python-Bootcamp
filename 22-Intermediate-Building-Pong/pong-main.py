2d39bcadbeea17128e1f4960e901db602b1ba9f6
Martin Littlecott <mlittlec@hotmail.com>
2022-01-17 21:03:55 +0000

    Update pong-main.py

--- 22-Intermediate-Building-Pong/pong-main.py
+++ 22-Intermediate-Building-Pong/pong-main.py
@@ -1,10 +1,19 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
 
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(strech_wid=5, strech_len=1)
+paddle.penup()
+paddle.goto(350,0)
+
+screen.listen()
+screen.onkey(go_up, "Up")
 
 
 screen.exitonclick()
