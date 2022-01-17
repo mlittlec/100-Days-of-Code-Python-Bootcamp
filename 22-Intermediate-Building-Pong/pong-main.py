a7e185e6da3b6b7c5385fc7287b06bf8648695ed
Martin Littlecott <mlittlec@hotmail.com>
2022-01-17 21:48:11 +0000

    Update pong-main.py

--- 22-Intermediate-Building-Pong/pong-main.py
+++ 22-Intermediate-Building-Pong/pong-main.py
@@ -1,5 +1,8 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -7,34 +10,38 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-r_paddle = Paddle(350, 0)
-l_paddle = Paddle(-350, 0)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
 
-# paddle = Turtle()
-# paddle.shape("square")
-# paddle.color("white")
-# paddle.shapesize(strech_wid=5, strech_len=1)
-# paddle.penup()
-# paddle.goto(350,0)
-#
-#
-# def go_up():
-#     new_y = paddle.ycor() + 20
-#     paddle.goto(paddle.xcor, new_y)
-#    
-#    
-# def go_down():
-#     new_y = paddle.ycor() - 20
-#     paddle.goto(paddle.xcor, new_y)
-#
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
-
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
     screen.update()
+    ball.move()
 
+    #Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    #Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    #Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    #Detect L paddle misses:
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
