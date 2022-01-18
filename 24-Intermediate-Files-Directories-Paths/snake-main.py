91cac49ef5e6c8b7701820029b7b9f8f22fdfacf
Martin Littlecott <mlittlec@hotmail.com>
2022-01-18 20:07:26 +0000

    Update snake-main.py

--- 24-Intermediate-Files-Directories-Paths/snake-main.py
+++ 24-Intermediate-Files-Directories-Paths/snake-main.py
@@ -34,16 +34,16 @@ while game_is_on:
 
     #Detect collision with wall.
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        scoreboard.reset()
-        snake.reset()
+        game_is_on = False
+        scoreboard.game_over()
 
     #Detect collision with tail.
     for segment in snake.segments:
         if segment == snake.head:
             pass
         elif snake.head.distance(segment) < 10:
-            scoreboard.reset()
-            snake.reset()
+            game_is_on = False
+            scoreboard.game_over()
 
 
 
