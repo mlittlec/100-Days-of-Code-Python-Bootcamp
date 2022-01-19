05dffe0fa0a955cb75c8e0101d3f58e978a85213
Martin Littlecott <mlittlec@hotmail.com>
2022-01-19 21:15:06 +0000

    Update squirrel-data.py

--- 25-Intermediate-CSV-and_Pandas/squirrel-data.py
+++ 25-Intermediate-CSV-and_Pandas/squirrel-data.py
@@ -7,3 +7,13 @@ black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
 print(grey_squirrels_count)
 print(red_squirrels_count)
 print(black_squirrels_count)
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrel_count, red_squirrel_count, black_squirrel_count]
+}
+
+print(data_dict)
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
