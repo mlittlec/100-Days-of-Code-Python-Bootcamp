9ec917d18fdfc2ef9c1c300795351042ec4fb6f5
Martin Littlecott <mlittlec@hotmail.com>
2022-01-19 23:00:44 +0000

    Update main.py

--- 25-Intermediate-List-and_Dictionary-Comprehensions/Exercise-01/main.py
+++ 25-Intermediate-List-and_Dictionary-Comprehensions/Exercise-01/main.py
@@ -3,10 +3,9 @@ numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
 #Write your 1 line code 👇 below:
 
-
+squared_numbers = [num**2 for num in numbers]
 
 #Write your code 👆 above:
 
 print(squared_numbers)
-
 
