2f08f4ee6612d26e89343d1bc73eaf4e52610b17
Martin Littlecott <mlittlec@hotmail.com>
2022-01-20 22:58:24 +0000

    Create dictionary-comprehension.py

---
+++ 26-Intermediate-List-and_Dictionary-Comprehensions/dictionary-comprehension.py
@@ -0,0 +1,5 @@
+names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'freddie']
+import random
+students_scores = {student:random.randint(1, 100 for student in names}
+passed_students = {student:score for (student, score) in students_scores.items() if score >= 60}
+
