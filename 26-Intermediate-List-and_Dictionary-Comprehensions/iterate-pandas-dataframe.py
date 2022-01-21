5689e5ba20488f6b5578ad69f3b28c9179b074db
Martin Littlecott <mlittlec@hotmail.com>
2022-01-21 21:46:17 +0000

    Create iterate-pandas-dataframe.py

---
+++ 26-Intermediate-List-and_Dictionary-Comprehensions/iterate-pandas-dataframe.py
@@ -0,0 +1,17 @@
+student_dict = {
+    "Student": ["Angela", "James", "Lily"],
+    "Score": [56, 76, 98]
+}
+
+# Looping through dictionaries
+for (key, value) in Student)dict.items():
+    print(key)
+    
+ import pandas
+
+student_data_frame = pandas.Dataframe(student_dict)
+print(student_data_frame)
+
+# Loop through a data frame
+for (key, value) in student_data_frame.items():
+    print(value)
