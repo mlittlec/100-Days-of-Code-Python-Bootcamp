e6ff4f33bb5da835fbb3c2e5af382a709d1f9027
Martin Littlecott <mlittlec@hotmail.com>
2022-01-21 22:51:54 +0000

    Create simple-fui-main.py

---
+++ 27-Intermediate-args-andkwargs/simple-fui-main.py
@@ -0,0 +1,12 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("My first GUI program")
+window.minsize(width=500, height=300)
+
+# Label
+my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "Bold")
+my_label.pack(side="left")
+
+
+window.mainloop()
