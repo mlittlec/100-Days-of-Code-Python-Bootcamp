392bcefa06213fcccaa2a614b455be649401e054
Martin Littlecott <mlittlec@hotmail.com>
2022-01-21 21:31:08 +0000

    Create dict-comprehension-01.py

---
+++ 26-Intermediate-List-and_Dictionary-Comprehensions/dict-comprehension-01.py
@@ -0,0 +1,8 @@
+sentence = "What is the Airespeed Velocity of an Unladen Swallow?"
+# Don't change the code above
+
+# Write your code below
+result = {word:len(word) for word in sentence.split()}
+
+
+print(result)
