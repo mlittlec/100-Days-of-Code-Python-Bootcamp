90619e1b1b90142ed0dec8e7c26853bf9f5f18a4
Martin Littlecott <mlittlec@hotmail.com>
2022-01-22 16:40:05 +0000

    Update simple-fui-main.py

--- 27-Intermediate-args-andkwargs/simple-fui-main.py
+++ 27-Intermediate-args-andkwargs/simple-fui-main.py
@@ -1,4 +1,4 @@
-import tkinter
+import tkinter import *
 
 window = tkinter.Tk()
 window.title("My first GUI program")
@@ -7,6 +7,17 @@ window.minsize(width=500, height=300)
 # Label
 my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "Bold")
 my_label.pack(side="left")
+
+my_label["text"] = "New text"
+my_label.config(text="New Text")
+
+# Button
+def button_clicked():
+    print("I go clicked")
+
+button = tkinter.Button(text="Click Me", command=button_clicked)
+button.psck()
+
 
 
 window.mainloop()
