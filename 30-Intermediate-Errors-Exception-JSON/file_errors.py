71c88401b1760bc4dfb222abaab98556e5b62b11
Martin Littlecott <martin.littlecott@btinternet.com>
2022-01-23 16:49:34 +0000

    Submit recent updates

--- 30-Intermediate-Errors-Exception-JSON/file_errors.py
+++ 30-Intermediate-Errors-Exception-JSON/file_errors.py
@@ -1,4 +1,16 @@
 # Try to open a file that doesn't exist - to raise an exception ;-)
-with open("a_file.txt") as file:
-    file.read()
-    
+# FileNotFound error
+# with open("a_file.txt") as file:
+#     file.read()
+try:
+    file = open("a_file.txt")
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["key"])
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("Something")
+except KeyError as error_message:
+    print(f"That key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
