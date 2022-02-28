128a642556363992eabb5f6433c37c0a4c161e46
Martin Littlecott <martin.littlecott@btinternet.com>
2022-02-28 20:58:45 +0000

    Submit recent updates

--- Day01-14-Beginner/11-beginner-blackjack-project/password_manager_main.py
+++ Day01-14-Beginner/11-beginner-blackjack-project/password_manager_main.py
@@ -51,7 +51,7 @@ window.title("Password Manager")
 window.config(padx=50, pady=50)
 
 canvas = Canvas(height=200, width=200)
-logo_img = PhotoImage(file="../../29-Intermediate-Password-Manager-Project/logo.png")
+logo_img = PhotoImage(file="../../Day15-57-Intermediate/29-Intermediate-Password-Manager-Project/logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
