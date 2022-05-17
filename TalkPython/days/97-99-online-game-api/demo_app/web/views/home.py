59ec159fbb193863dd290d101f683e73fa24e8f2
Martin Littlecott <mlittlec@hotmail.com>
2022-05-17 17:27:40 +0100

    Create home.py

---
+++ TalkPython/days/97-99-online-game-api/demo_app/web/views/home.py
@@ -0,0 +1,12 @@
+import flask
+
+
+def build_views(app):
+    @app.route('/')
+    def index():
+        return "Hello world!!!"
+
+    @app.errorhandler(404)
+    def not_found(_):
+        return flask.Response("The page was not found.", status=404)
+      
